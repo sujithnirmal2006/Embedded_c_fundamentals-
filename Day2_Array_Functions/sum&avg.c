ed6c026e3c048ce2adecc30c38a3b33fabd4dc78
sujithnirmal2006 <sujithnirmal.bm23@bitsathy.ac.in>
2026-01-10 11:51:11 +0530

    Create sum&avg.c

---
+++ Day2_Array_Functions/sum&avg.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n,sum=0,avg=0;
+    printf("size:");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+    }
+    avg=sum/n;
+    printf("sum=%d\n",sum);
+    printf("average=%d",avg);
+}
