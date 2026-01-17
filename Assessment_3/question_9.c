8491829c1b4cd8d865b72881d3658c5583dba6c3
sujithnirmal2006 <sujithnirmal.bm23@bitsathy.ac.in>
2026-01-17 08:28:42 +0530

    Create question_9.c

---
+++ Assessment_3/question_9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    b=a%10;
+    c=a/10;
+    if(b<c)
+    {
+        printf("Sucssess");
+    }
+    else
+    printf("Failure");
+}
