9561ff76c7ffd4287dd8b13a5c44c486dea80f76
sujithnirmal2006 <sujithnirmal.bm23@bitsathy.ac.in>
2026-01-18 09:12:46 +0530

    Create question_5.c

---
+++ Assessment_7/question_5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ void disp_odd();
+ int a,b,c;
+int main()
+{
+    disp_odd();
+}
+void disp_odd()
+{
+    while(a<10)
+    {
+       a++;
+       if(a%2==1){
+       printf("%d\n",a);}
+    } 
+}
