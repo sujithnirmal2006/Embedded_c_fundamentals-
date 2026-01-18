bf50fce55150482f026bfcf00921c687bd21dea8
sujithnirmal2006 <sujithnirmal.bm23@bitsathy.ac.in>
2026-01-18 09:11:31 +0530

    Create question_3.c

---
+++ Assessment_7/question_3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ void disp_sum();
+ int a,b,c;
+int main()
+{
+    disp_sum();
+}
+void disp_sum()
+{
+    while(a<5)
+    {
+       a++;
+       b=b+a;
+    } printf("%d\n",b);
+}
