f89bb82b73e6f8c200abc76a32094627a6f02a20
sujithnirmal2006 <sujithnirmal.bm23@bitsathy.ac.in>
2026-01-18 09:21:57 +0530

    Create question_19.c

---
+++ Assessment_7/question_19.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void check_prime(int x);
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    a=a/10;
+    a=a%100;
+    check_prime(a);
+}
+ void check_prime(int x)
+{
+    int y=0;
+    if(x==2)
+    {
+        printf("Prime");
+    }
+    else
+    {
+        for(int i=2;i<=x/2;i++)
+        {
+            if(x%i==0)
+            {
+              y=1;
+              break;
+            }
+        }if(y==0)
+        {
+            printf("prime");
+        }
+        else
+        {
+            printf("not prime");
+        }
+    }
+}
