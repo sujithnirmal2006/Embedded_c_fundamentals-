49dc4f391682714b327e60fe6b321c092d9c9863
sujithnirmal2006 <sujithnirmal.bm23@bitsathy.ac.in>
2026-01-18 09:12:07 +0530

    Create question_4.c

---
+++ Assessment_7/question_4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ void disp_rsum();
+ int a,b,c;
+int main()
+{
+    disp_rsum();
+}
+void disp_rsum()
+{
+    while(a<6)
+    {
+       a++;
+       b=b+a;
+    } printf("%d\n",b);
+}
